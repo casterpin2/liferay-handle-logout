3cb7c549a3868c77ed8e33c1589a6e9bbc8523a4
casterpin2 <casterpin2@gmail.com>
2025-07-03 22:09:25 +0700

    TEST

--- modules/oauth2-revoke/src/main/java/com/example/oauth2/revoke/application/RevokeTokenResourceApplication.java
+++ modules/oauth2-revoke/src/main/java/com/example/oauth2/revoke/application/RevokeTokenResourceApplication.java
@@ -1,16 +1,18 @@
 package com.example.oauth2.revoke.application;
 
+
 import java.util.Collections;
 import java.util.Set;
-
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import com.liferay.oauth2.provider.model.OAuth2Authorization;
 import com.liferay.oauth2.provider.service.OAuth2AuthorizationLocalServiceUtil;
 import com.liferay.oauth2.provider.service.OAuth2AuthorizationService;
+import com.liferay.portal.kernel.cache.CacheRegistryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
@@ -20,8 +22,12 @@ import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
  */
 @Component(
 	property = {
-		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/greetings",
-		JaxrsWhiteboardConstants.JAX_RS_NAME + "=Greetings.Rest"
+		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/account-logout",
+		JaxrsWhiteboardConstants.JAX_RS_NAME + "=RevokeTokenResource",
+			// Ép dùng OAuth2 verifier, tắt BasicAuth
+			"osgi.jaxrs.extension.select=(osgi.jaxrs.name=Liferay.OAuth2)",
+			// Vô hiệu hóa scope checking
+			"oauth2.scopechecker.type=none"
 	},
 	service = Application.class
 )
@@ -39,31 +45,40 @@ public class RevokeTokenResourceApplication extends Application {
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response revoke(
-			@FormParam("token") String token,
-			@FormParam("token_type_hint") String tokenTypeHint
+			@Context HttpServletRequest request
 	) {
 		// Tìm authorization bằng access token
-		OAuth2Authorization auth =
-				OAuth2AuthorizationLocalServiceUtil.
-						fetchOAuth2AuthorizationByAccessTokenContent(token);
-
-		if (auth == null) {
-			return Response
-					.status(Response.Status.BAD_REQUEST)
-					.entity(Collections.singletonMap("error", "invalid_token"))
-					.build();
-		}
-
-		// Gọi service để thu hồi token
         try {
+			String authHeader = request.getHeader("Authorization");
+			if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+				return Response
+						.status(Response.Status.UNAUTHORIZED)
+						.entity(Collections.singletonMap("error", "missing_or_invalid_authorization_header"))
+						.build();
+			}
+
+			// Bóc token ra (bỏ chữ "Bearer ")
+			String bearerToken = authHeader.substring("Bearer ".length());
+			OAuth2Authorization auth =
+					OAuth2AuthorizationLocalServiceUtil.
+							fetchOAuth2AuthorizationByAccessTokenContent(bearerToken);
+
+			if (auth == null) {
+				return Response
+						.status(Response.Status.BAD_REQUEST)
+						.entity(Collections.singletonMap("error", "invalid_token"))
+						.build();
+			}
             _oAuth2AuthorizationService.revokeOAuth2Authorization(
                     auth.getOAuth2AuthorizationId()
             );
+			CacheRegistryUtil.clear();
+
         } catch (PortalException e) {
             throw new RuntimeException(e);
         }
 
-        return Response.ok().build();
+        return Response.status(Response.Status.OK).entity(Collections.singletonMap("success", "Logout successfully")).build();
 	}
 
 }
